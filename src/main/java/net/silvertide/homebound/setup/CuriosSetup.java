d450c007764292158c4397d99f27729bde7d2cc9
Silvertide7 <natephillips801@gmail.com>
2025-02-23 22:58:46 -0700

    Undo merge from main

---
+++ src/main/java/net/silvertide/homebound/setup/CuriosSetup.java
@@ -0,0 +1,16 @@
+package net.silvertide.homebound.setup;
+
+import net.neoforged.fml.ModList;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.silvertide.homebound.compat.CuriosCompat;
+
+public class CuriosSetup {
+
+    public static void init(final FMLCommonSetupEvent ignored) {
+        if(ModList.get().isLoaded("curios")) {
+            CuriosCompat.isCuriosLoaded = true;
+            NeoForge.EVENT_BUS.addListener(CuriosCompat::keepCurios);
+        }
+    }
+}
